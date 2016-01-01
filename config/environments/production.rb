833199c9e11e15651e9cf4d75f6cf267dbd4989a
Jim Raynor <jim_raynor24@yahoo.com>
2016-01-01 17:52:54 +0800

    Revert the change for relative_url_root as it breaks in heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,4 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # set the relative root, because we're deploying to /esahod
-  config.action_controller.relative_url_root = '/esahod'
 end
